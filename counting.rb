947fe951074a03dc9ee7f37bf89b3a6d6a11618e
David Hail <dhail@ymail.com>
2016-09-28 08:53:46 -0700

    Added advanced option to counting game

--- counting.rb
+++ counting.rb
@@ -1,5 +1,4 @@
 
-
 def count(total_number, friends)
 
 	number = 1
@@ -41,3 +40,56 @@ def count(total_number, friends)
 end
 
 count(100, ["Jim", "Garrett", "Sammie", "Dave", "Will", "Bubba", "Billy", "Sabrina", "Fake Billy", "Fat Garrett"])
+
+=begin
+Advanced version follows
+=end
+
+def printit(friends, total_number)
+	
+	number = 1
+	direction = 1
+	friend = 1
+
+	while number <= total_number
+
+		puts "Friend #{friend} says #{number}"
+
+		if number % 7 == 0
+			if direction == 1
+				direction = -1
+			else
+				direction = 1
+			end
+		end
+
+		if number % 11 == 0
+			if direction == 1 && friend == friends
+				friend = 1
+			elsif direction == -1 && friend == 1
+				friend = friends				
+			else
+				friend += direction
+			end
+		end
+
+		if direction == 1 && friend == friends
+			friend = 1
+		elsif direction == -1 && friend == 1
+			friend = friends
+		else
+			friend += direction
+		end
+
+		number += 1
+	end
+
+end
+
+puts "Enter the number of friends"
+counters = gets.chomp.to_i
+
+puts "How high do you want them to count?"
+count = gets.chomp.to_i
+
+printit(counters, count)
